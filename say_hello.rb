b3a149c805d7ae6fb34b8fc986137a2192071d4f
chic-caller-7520 <arbrown110@gmail.com>
2020-03-24 22:26:07 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
-def say_hello(name="Kent Beck!")
-puts "Hello  #{name}"
+def say_hello(name="Kent Beck")
+puts "Hello  #{name}!"
 end
 
-def say_hello(name="Ruby Programmer!")
-  puts "Hello #{name}"
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
 end
